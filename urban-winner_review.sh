bdd0bde51206a95edf76de1f2a51ae495ad1b035
bigmantate <35516914+bigmantate@users.noreply.github.com>
2018-02-13 15:57:29 -0600

    Create urban-winner_review.sh

---
+++ urban-winner_review.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+
+
+#Looks At Usb Devices 
+
+
+
+
+
+
+
+echo MONITORING USB DEVICES 
+
+
+
+
+
+
+
+udevadm monitor
+
+
+
+
+
+
+
+chmod x
+
+
+
+-----------------------
+
+#Review urban-winner
+
+
+
+#When I ran the script I had to exit out of udevadm command, mabey you could create some comments instructing the user
+
+#they may have to do this otherwise it stays on to monitor mode
+
+
+
+#Also chmod x is saying it is missing operand after 'x' so I think it is missing some of the argument
